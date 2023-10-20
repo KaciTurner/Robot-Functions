3cfa6076a5621dc8484039b11a21cabb731901cd
KaciTurner <143512055+KaciTurner@users.noreply.github.com>
2023-10-20 09:51:40 -0400

    Update code.py

--- code.py
+++ code.py
@@ -14,8 +14,8 @@ pwm_Lb = pwmio.PWMOut(left_motor_backward, frequency=10000)
 Left_Motor = motor.DCMotor(pwm_La, pwm_Lb)
 Left_Motor_speed = 0
 
-right_motor_forward = board.GP14
-right_motor_backward = board.GP15
+right_motor_forward = board.GP15
+right_motor_backward = board.GP14
 
 pwm_Ra = pwmio.PWMOut(right_motor_forward, frequency=10000)
 pwm_Rb = pwmio.PWMOut(right_motor_backward, frequency=10000)
@@ -23,82 +23,46 @@ pwm_Rb = pwmio.PWMOut(right_motor_backward, frequency=10000)
 Right_Motor = motor.DCMotor(pwm_Ra, pwm_Rb)
 Right_Motor_speed = 0
 
-
-while True:
-
-    Left_Motor_speed = -1
+def Robot_foward():
+    Left_Motor_speed = 1 
     Left_Motor.throttle = Left_Motor_speed
-    time.sleep(2)
-    Left_Motor_speed = 1
-    Left_Motor.throttle = Left_Motor_speed
-    time.sleep(2)
-
-    Right_Motor_speed = -1
-    Right_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
     Right_Motor_speed = 1
     Right_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
 
-
-left_motor_left = board.GP12
-left_motor_right = board.GP13
-
-pwm_La = pwmio.PWMOut(left_motor_left, frequency=10000)
-pwm_Lb = pwmio.PWMOut(left_motor_right, frequency=10000)
-
-Left_Motor = motor.DCMotor(pwm_La, pwm_Lb)
-Left_Motor_speed = 0
-
-right_motor_left = board.GP14
-right_motor_right = board.GP15
-
-pwm_Ra = pwmio.PWMOut(right_motor_left, frequency=10000)
-pwm_Rb = pwmio.PWMOut(right_motor_right, frequency=10000)
-
-Right_Motor = motor.DCMotor(pwm_Ra, pwm_Rb)
-Right_Motor_speed = 0
-
-def Robot_foward():
-    Left_Motor_speed = -1
-    Left_Motor.throttle = Left_Motor_speed
-    time.sleep(2)
-    Right_Motor_speed = -1
-    Right_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
-
-
-while True:
 def Robot_backward():
     Left_Motor_speed = -1
-    Left_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
+    Left_Motor.throttle = Left_Motor_speed
+    Right_Motor_speed = -1
+    Right_Motor.throttle = Right_Motor_speed
+    
+def Robot_left():
+   # Left_Motor_speed = 1
+   # Left_Motor.throttle = Right_Motor_speed
     Right_Motor_speed = 1
     Right_Motor.throttle = Right_Motor_speed
-    time.sleep(2
-
-    def Robot_left():
+ 
+def Robot_right():
     Left_Motor_speed = 1
-    Left_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
-    Right_Motor_speed = 0.1
-    Right_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
+    Left_Motor.throttle = Left_Motor_speed
+    #Right_Motor_speed = 1
+    #Right_Motor.throttle = Right_Motor_speed
 
-
-    def Robot_right():
-    Left_Motor_speed = -1
-    Left_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
-    Right_Motor_speed = 1
-    Right_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
-
-    def Robot_stop():
+def Robot_stop():
     Left_Motor_speed = 0
-    Left_Motor.throttle = Right_Motor_speed
-    time.sleep(2)
+    Left_Motor.throttle = Left_Motor_speed
     Right_Motor_speed = 0
     Right_Motor.throttle = Right_Motor_speed
+    
+while True:
+    Robot_foward()
+    time.sleep(5)
+    Robot_left()
     time.sleep(2)
+    Robot_forward()
+    time.sleep(3)
+   
+   
 
+
+
+# Write your code here :-)
